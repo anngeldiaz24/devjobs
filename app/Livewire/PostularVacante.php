3ca8c36714eb072abc3a126c2e36d76444b3d8ae
Angel Diaz <angel.diaz5609@alumnos.udg.mx>
2024-03-09 13:28:21 -0600

    commit: creando candidato y almacenandolo en la tabla

--- app/Livewire/PostularVacante.php
+++ app/Livewire/PostularVacante.php
@@ -2,22 +2,41 @@
 
 namespace App\Livewire;
 
+use App\Models\Vacante;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class PostularVacante extends Component
 {
 
+    use WithFileUploads;
     public $cv;
+    public $vacante;
 
     protected $rules = [
         'cv' => 'required|mimes:pdf'
     ];
 
+    public function mount(Vacante $vacante)
+    {
+        $this->vacante = $vacante;
+    }
+
     public function postularme()
     {
-        $this->validate();
-        
+        $datos = $this->validate();
+
         //Store cv in the server
+        $cv = $this->cv->store('public/cv');
+        //Elimina el directorio y unicamente guardamos el nombre del archivo
+        $datos['cv'] = str_replace('public/cv/', '', $cv);
+        //dd($nombre_imagen);
+
+        //Create the candidate 
+        $this->vacante->candidatos()->create([
+            'user_id' => auth()->user()->id,
+            'cv' => $datos['cv'],
+        ]);
 
         //Create a notificación and send email
 
